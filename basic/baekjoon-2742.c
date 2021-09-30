c190f705a885de438d137da4e81912f306141d88
Junseong Woo <80818534+whitebear05@users.noreply.github.com>
2021-09-30 11:31:59 +0900

    Create baekjoon-2742.c

---
+++ basic/baekjoon-2742.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = n; i > 0; i--)
+	{
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
