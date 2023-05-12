a15a2052d6b9f993dcb0512aada675950dec5baf
whitebear05 <woojs0505@naver.com>
2023-05-12 09:37:52 +0900

    python: 프로그래머스 성격 유형 검사하기

--- python/baekjoon-26215.py
+++ python/baekjoon-26215.py
@@ -1,13 +1,22 @@
 import sys
 
 n = int(input())
-houses = sorted(map(int, input().split()), reverse=True)
+houses = sorted(map(int, input().split()))
 answer = 0
 
 if houses[0] > 1440:
-    sys.exit("-1")
+    sys.exit("-1") # -1 출력과 함께 프로그램 종료
+
+if n == 1:
+    sys.exit("1")
 
 while True:
+    houses.sort(reverse=True)
+    
+    if houses == []:
+        print(answer)
+        break
+    
     houses[0] -= 1
     if len(houses) > 1 and houses[1] != 0:
         houses[1] -= 1
@@ -17,8 +26,4 @@ while True:
     
     if answer > 1440:
         print("-1")
-        break
-    
-    if houses == []:
-        print(answer)
         break
